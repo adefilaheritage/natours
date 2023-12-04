b07bf859d9befc150ede5230b1c23ab191c9b8f8
heritage <adefila.inioluwa@gmail.com>
2023-12-04 18:33:35 +0100

    Password Reset Functionality

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 // const slugify = require('slugify');
 const validator = require('validator');
@@ -25,6 +26,11 @@ const userSchema = new mongoose.Schema({
         // minlength: [3, 'An email address must have more than or equal to 3 characters'],
     },
     photo: String,
+    role: {
+        type: String,
+        enum: ['user', 'guide', 'lead-guide', 'admin'],
+        default: 'user'
+    },
     password: {
         type: String,
         required: [true, 'Please provide a password'],
@@ -42,7 +48,9 @@ const userSchema = new mongoose.Schema({
             message: 'Passwords are not the same!'
         }
     },
-    passwordChangedAt: Date
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -70,7 +78,24 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
 
     // False means NOT changed
     return false;
-}
+};
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+        console.log({resetToken}, this.passwordResetToken);
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+};
 
 const User = mongoose.model('User', userSchema);
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
-const authcontroller = require('./../controllers/authController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
@@ -19,12 +19,16 @@ router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router.route('/')
-    .get(authcontroller.protect, tourController.getAllTours)
+    .get(authController.protect, tourController.getAllTours)
     .post(tourController.createTour);
 
 router.route('/:id')
     .get(tourController.getTour)
     .patch(tourController.updateTour)
-    .delete(tourController.deleteTour);
+    .delete(
+        authController.protect,
+        authController.restrictTo('admin', 'lead-guide'),
+        tourController.deleteTour
+    );
 
 module.exports = router;
